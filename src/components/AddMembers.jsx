74ca4460f7135c589dcfbb930ab95a56af370d5a
Aria <christina.ohari@gmail.com>
2022-11-10 09:14:24 +0900

    [프론트엔드] 백엔드 API 연동

--- src/components/AddMembers.jsx
+++ src/components/AddMembers.jsx
@@ -1,24 +1,40 @@
 import { InputTags } from "react-bootstrap-tagsinput"
-import { useRecoilState, useRecoilValue } from "recoil"
+import { useSetRecoilState } from "recoil"
 import { CenteredOverlayForm } from "./shared/CenteredOverlayForm"
 import { groupMembersState } from "../state/groupMembers"
 import { useState } from "react"
-import { groupNameState } from "../state/groupName"
 import { useNavigate } from "react-router-dom"
-import { ROUTES } from "../routes"
+import { ROUTE_UTILS } from "../routes"
 import styled from "styled-components"
+import { API } from "aws-amplify"
+import { useGroupData } from "../hooks/useGroupData"
 
 export const AddMembers = () => {
-  const [groupMembers, setGroupMembers] = useRecoilState(groupMembersState)
-  const groupName = useRecoilValue(groupNameState)
+  const { groupId, groupName, groupMembers } = useGroupData()
+
+  const setGroupMembers = useSetRecoilState(groupMembersState)
   const [validated, setValidated] = useState(false)
   const navigate = useNavigate()
+
+  const saveGroupMembers = () => {
+    API.put('groupsApi', `/groups/${groupId}/members`, {
+      body: {
+        members: groupMembers
+      }
+    })
+      .then(_response => {
+        navigate(ROUTE_UTILS.EXPENSE_MAIN(groupId))
+      })
+      .catch(({ response }) => {
+        alert(response)
+      })
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     setValidated(true)
     if (groupMembers.length > 0) {
-      navigate(ROUTES.EXPENSE_MAIN)
+      saveGroupMembers()
     }
   }
 
@@ -31,6 +47,7 @@ export const AddMembers = () => {
       handleSubmit={handleSubmit}
     >
       <InputTags
+        values={groupMembers}
         data-testid="input-member-names"
         placeholder="이름 간 띄어 쓰기"
         onTags={(value) => setGroupMembers(value.values)}
---
+++ src/hooks/useGroupData.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { API } from "aws-amplify"
+import { useRecoilState } from "recoil"
+import { groupNameState } from "../state/groupName"
+import { groupIdState } from "../state/groupId"
+import { groupMembersState } from "../state/groupMembers"
+import { expensesState } from "../state/expenses"
+
+export const useGroupData = () => {
+  const { guid } = useParams()
+  const [groupName, setGroupName] = useRecoilState(groupNameState)
+  const [groupId, setGroupId] = useRecoilState(groupIdState)
+  const [groupMembers, setMembers] = useRecoilState(groupMembersState)
+  const [expenses, setExpenses] = useRecoilState(expensesState)
+
+  const fetchAndSetGroupData = () => {
+    API.get('groupsApi', `/groups/${guid}`)
+      .then(({data}) => {
+        setGroupName(data.groupName)
+        setGroupId(data.guid)
+        setMembers(data.members)
+        setExpenses(data.expenses || [])
+      })
+      .catch(error => {
+        alert("데이터를 불러오는데 실패 했습니다.")
+        console.log(error.response)
+      })
+  }
+
+  useEffect(() => {
+    if (guid?.length > 0) {
+      fetchAndSetGroupData()
+    }
+  }, [guid])
+
+  return {
+    groupId,
+    groupName,
+    groupMembers,
+    expenses,
+  }
+
+}
