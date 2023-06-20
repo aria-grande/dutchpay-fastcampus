f7d28cd9b5305a8352d7b2d884c8bfbe73a32952
aria-grande <christina.ohari@gmail.com>
2023-06-20 16:10:43 -0400

    Frontend - support deleting an expense from expense list

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,14 +1,38 @@
 import { Table } from "react-bootstrap"
-import { useRecoilValue } from "recoil"
+import { useRecoilValue, useSetRecoilState } from "recoil"
 import styled from "styled-components"
 import { currencyState } from "../state/currency"
 import { expensesState } from "../state/expenses"
 import { getDescriptiveAmount } from "../util"
 import { OverlayWrapper } from "./shared/OverlayWrapper"
-
+import {API} from "aws-amplify"
+import {groupIdState} from "../state/groupId"
+import {TrashFill} from "react-bootstrap-icons"
 export const ExpenseTable = () => {
+  const guid = useRecoilValue(groupIdState)
   const expenses = useRecoilValue(expensesState)
   const currency = useRecoilValue(currencyState)
+  const setExpense = useSetRecoilState(expensesState)
+
+  const deleteExpense = (expenseIdx) => {
+    // eslint-disable-next-line no-restricted-globals
+    if (!confirm("해당 비용을 정말 삭제 하시겠습니까?")) {
+      return ;
+    }
+    const newExpenses = expenses.filter((_, idx) => idx !== expenseIdx)
+
+    API.put('groupsApi', `/groups/${guid}/expenses/replace`, {
+      body: {
+        expenses: newExpenses
+      }
+    })
+        .then(_response => {
+          setExpense(expenses => newExpenses)
+        })
+        .catch(_error => {
+          alert("비용 삭제에 실패 했습니다. 다시 시도해 주세요.")
+        })
+  }
 
   return (
     <OverlayWrapper minHeight={"73vh"}>
@@ -19,6 +43,7 @@ export const ExpenseTable = () => {
             <th>내용</th>
             <th>결제자</th>
             <th>금액</th>
+            <th></th>
           </tr>
         </StyledThead>
         <StyledBody>
@@ -28,6 +53,11 @@ export const ExpenseTable = () => {
             <td>{desc}</td>
             <td>{payer}</td>
             <td>{getDescriptiveAmount(currency, amount)}</td>
+            <td>
+              <StyledAnchor onClick={() => deleteExpense(idx)}>
+                <TrashFill/>
+              </StyledAnchor>
+            </td>
           </tr>
         ))}
         </StyledBody>
@@ -35,6 +65,14 @@ export const ExpenseTable = () => {
     </OverlayWrapper>
   )
 }
+
+const StyledAnchor = styled.a`
+  cursor: pointer;
+  color: dimgray;
+  &:hover {
+    color: indianred;
+  }
+`
 
 const StyledTable = styled(Table)`
   min-width: 450px;
